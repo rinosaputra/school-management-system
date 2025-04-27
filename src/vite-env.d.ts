d255da5ec4ee4d5e6879b26cdd811d24f99b4953
Rino Saputra <rinosaputraas@gmail.com>
2025-04-27 13:20:01 +0700

    firestore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // Environment variables for application
+  readonly VITE_APP_NAME_SHORT: string;
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_VERSION: string;
+
+  // Firebase configuration
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
